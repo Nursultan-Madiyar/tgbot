492cb165948a2c91350a47514d4a262c35dad326
MNursultan <MNursultan@beeline.kz>
2022-02-16 17:40:26 +0600

    Initial commit

---
+++ src/main/java/kz/practice/tgbot/dto/GetCursOnDateXmlResult.java
@@ -0,0 +1,17 @@
+package kz.practice.tgbot.dto;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "GetCursOnDateXmlResult")
+@Data
+public class GetCursOnDateXmlResult {
+
+    @XmlElementWrapper(name = "ValueData", namespace = "")
+    @XmlElement(name = "ValuteCursOnDate", namespace = "")
+    private List<ValuteCursOnDate> valueData = new ArrayList<>();
+}
